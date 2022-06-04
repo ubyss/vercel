42f4a99ecbe6f7ac1cc1d7d0c4311f3179d7c83e
Thiago Vinícius <thiagojrtb@gmail.com>
2022-06-04 09:45:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World new changes for deploy</p>
       </header>
     </div>
   );
